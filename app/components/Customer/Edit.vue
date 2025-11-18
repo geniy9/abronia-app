fb279266ccee62c450aeaf92fb895de2f6b1dc41
geniy9 <geniy9@gmail.com>
2025-11-19 02:35:08 +0500

    ci: CRUD customers with repeatable contacts

--- app/components/Customer/Edit.vue
+++ app/components/Customer/Edit.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { z } from 'zod'
 
 const { update } = useStrapi()
@@ -97,6 +97,164 @@ const isDisabled = computed(() => { return !data.name })
         <RemoveEntry 
           :id="id" api="customers" 
           :entry-name="`клиент ${data.name}?`" 
+          @on-removed="onRemoved" />
+        <UFieldGroup>
+          <UButton :to="route.path" color="neutral" variant="soft" size="sm">
+            Отмена
+          </UButton>
+          <UButton 
+            type="submit" size="sm"
+            :disabled="isDisabled" 
+            :loading="data.loading">
+            Обновить
+          </UButton>
+        </UFieldGroup>
+      </div>
+    </UForm>
+  </UCard>
+</template> -->
+
+<script setup>
+import { z } from 'zod'
+
+const { update, findOne } = useStrapi()   
+const toast = useToast()
+const router = useRouter()
+const route = useRoute()
+const emit = defineEmits(['on-edited'])
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  customerData: {
+    type: Object,
+    default: null
+  },
+})
+
+const schema = z.object({
+  name: z.string().min(3, 'Укажите название компании или имя клиента'),
+  description: z.string().optional(),
+  website: z.string().optional(),
+  contact: z.array(z.object({
+    position: z.string().optional(),
+    name: z.string().min(1, 'Укажите имя контакта'),
+    phone: z.string().min(6, 'Укажите номер телефона').optional(), 
+    email: z.email('Неверный формат email').optional().or(z.literal('')),
+  })).optional()
+});
+
+const data = reactive({
+  name: props.customerData?.name || '',
+  description: props.customerData?.description || '',
+  website: props.customerData?.website || '',
+  contact: props.customerData?.contact && props.customerData.contact.length > 0 
+    ? JSON.parse(JSON.stringify(props.customerData.contact)) 
+    : [{ position: '', name: '', phone: '', email: '' }],
+  loading: false,
+})
+
+function addContact() {
+  data.contact.push({ position: '', name: '', phone: '', email: '' });
+}
+function removeContact(index) { data.contact.splice(index, 1) }
+
+async function onSubmit(event) {
+  data.loading = true;
+  try {
+    const customerPayload = { ...event.data }
+    
+    if (customerPayload.contact && customerPayload.contact.length) {
+      customerPayload.contact.forEach(person => {
+        if (person.position === '') person.position = null;
+        if (person.phone === '') person.phone = null;
+        if (person.email === '') person.email = null;
+      });
+    }
+
+    const responseCustomer = await update('customers', props.id, customerPayload);
+    if (responseCustomer?.data) {
+      const res = await findOne('customers', props.id, {
+        populate: { comment: true, contact: true }
+      })
+      toast.add({ 
+        title: 'Клиент успешно обновлен', 
+        color: 'success', 
+        icon: 'hugeicons:checkmark-circle-02'
+      });
+      emit('on-edited', res.data)
+    }
+
+  } catch (e) {
+    console.error(e); // Для отладки
+    toast.add({ 
+      title: 'Ошибка', 
+      description: 'Ошибка при обновлении клиента, попробуйте еще раз', 
+      color: 'error', 
+      icon: 'hugeicons:cancel-circle'
+    })
+  } finally {
+    data.loading = false;
+  }
+}
+function onRemoved() { router.push('/customers') }
+const isDisabled = computed(() => { return !data.name })
+</script>
+<template>
+  <UCard>
+    <UForm :schema="schema" :state="data" class="space-y-4" @submit="onSubmit">
+      <UFormField label="Клиент" name="name" required>
+        <UInput v-model="data.name" placeholder="Название компании / Имя клиента" class="w-full" />
+      </UFormField>
+      <UFormField label="Описание" name="description">
+        <UTextarea v-model="data.description" placeholder="Краткое описание деятельности" autoresize class="w-full" />
+      </UFormField>
+      <UFormField label="Сайт" name="website">
+        <UFieldGroup class="w-full">
+          <UBadge color="neutral" variant="outline" label="https://" />
+          <UInput v-model="data.website" placeholder="company.com" type="text" class="w-full" />
+        </UFieldGroup>
+      </UFormField>
+
+      <USeparator label="Контакты" class="my-6" />
+
+      <div v-for="(person, i) in data.contact" :key="i" class="p-4 border border-dashed border-gray-300 dark:border-gray-700 rounded-lg space-y-4 relative">
+        <div class="flex justify-between items-center">
+          <h3 class="font-semibold text-gray-900 dark:text-white">Контакт #{{ i + 1 }}</h3>
+          <UButton v-if="data.contact.length > 1"
+            icon="hugeicons:cancel-01" 
+            size="sm" 
+            color="error" 
+            square 
+            variant="soft"
+            @click="removeContact(i)" />
+        </div>
+        <UFormField label="Должность" :name="`contact.${i}.position`">
+          <UInput v-model="person.position" placeholder="Менеджер по продажам" class="w-full" />
+        </UFormField>
+        <UFormField label="Имя" :name="`contact.${i}.name`">
+          <UInput v-model="person.name" placeholder="Имя Фамилия" class="w-full" />
+        </UFormField>
+        <UFormField label="Телефон" :name="`contact.${i}.phone`">
+          <UInput v-model="person.phone" placeholder="+993 (XX) XX-XX-XX" class="w-full" />
+        </UFormField>
+        <UFormField label="Email" :name="`contact.${i}.email`">
+          <UInput v-model="person.email" placeholder="example@company.com" type="email" class="w-full" />
+        </UFormField>
+      </div>
+
+      <UButton 
+        label="Добавить контакт" 
+        icon="hugeicons:user-add-02" 
+        variant="outline"
+        @click="addContact" />
+      
+      <div class="flex items-center justify-between pt-4">
+        <RemoveEntry 
+          :id="id" api="customers" 
+          :entry-name="`клиент ${data.name}`" 
           @on-removed="onRemoved" />
         <UFieldGroup>
           <UButton :to="route.path" color="neutral" variant="soft" size="sm">
--- app/components/Customer/index.vue
+++ app/components/Customer/index.vue
@@ -1,111 +1,3 @@
-<!-- <script setup>
-const route = useRoute()
-const props = defineProps({
-  customer: {
-    type: Object,
-    default: null
-  }
-})
-
-const item = ref(props.customer)
-
-const commentPrompt = ref(null)
-const currentComment = ref({
-  message: props.customer?.comment?.message,
-  documentId: props.customer?.comment?.documentId
-})
-function openCommentPrompt() {
-  commentPrompt.value?.openCommentPrompt()
-}
-function handleComment(data) {
-  currentComment.value.message = data.message
-  currentComment.value.documentId = data.documentId
-}
-
-function onEdited(obj) { item.value = obj }
-const isEdit = computed(() => route.hash === '#edit')
-</script>
-<template>
-  <div class="flex flex-col w-full gap-2">
-
-    <div class="grid grid-cols-[auto_1fr_auto] items-center bg-primary text-white rounded-lg p-2">
-      <NuxtLink to="/customers" class="text-2xl leading-0 p-2">
-        <UIcon name="hugeicons:link-backward" />
-      </NuxtLink>
-      <div class="text-center">
-        <p class="text-xs">Клиент</p>
-        <h2 class="text-lg font-bold">{{ item.name }}</h2>
-      </div>
-      <NuxtLink :to="isEdit ? route.path : `${route.path}#edit`" class="text-2xl leading-0 p-2">
-        <UIcon :name="isEdit ? 'hugeicons:cancel-square' : 'hugeicons:settings-01'" />
-      </NuxtLink>
-    </div>
-
-    <CustomerEdit 
-      v-if="isEdit" 
-      :id="item.documentId" 
-      :customerData="item" 
-      @onEdited="onEdited" />
-    <div v-else class="flex flex-col gap-2 pb-2">
-      <div v-if="item.description" class="flex items-center justify-center">
-        <span class="text-center text-gray-900 dark:text-white text-sm">
-          {{ item.description }}
-        </span>
-      </div>
-      <div v-if="item.contactName" class="grid gap-1 bg-gray-200 dark:bg-gray-900 text-gray-900 dark:text-white rounded-lg p-3">
-        <span class="text-sm opacity-70">Контактное лицо</span>
-        <span class="font-bold">{{ item.contactName }}</span>
-      </div>
-      <div v-if="item.phone || item.email || item.website" class="grid gap-4 bg-gray-200 dark:bg-gray-900 rounded-lg p-3">
-        <div v-if="item.phone" class="flex flex-col gap-1 text-gray-900 dark:text-white">
-          <span class="text-sm opacity-70">Телефон</span>
-          <span class="font-bold">{{ item.phone }}</span>
-        </div>
-        <div v-if="item.email" class="flex flex-col gap-1 text-gray-900 dark:text-white">
-          <span class="text-sm opacity-70">Электронный адрес</span>
-          <span class="font-bold">{{ item.email }}</span>
-        </div>
-        <div v-if="item.website" class="flex flex-col gap-1 text-gray-900 dark:text-white">
-          <span class="text-sm opacity-70">Сайт</span>
-          <span class="font-bold">{{ item.website }}</span>
-        </div>
-      </div>
-      <UAccordion :items="[{
-          label: 'Комментарий',
-          icon: 'hugeicons:message-01',
-          trailingIcon: 'hugeicons:arrow-down-01',
-          slot: 'comment',
-        }]">
-        <template #comment>
-          <div v-if="currentComment.documentId" class="flex flex-col items-stretch gap-2 bg-gray-200 dark:bg-gray-800 rounded-md p-2">
-            <div v-html="currentComment.message" class="text-sm text-gray-700 dark:text-gray-400"></div>
-            <UButton :to="`/comments/${currentComment.documentId}`" 
-              color="neutral" variant="soft" 
-              icon="hugeicons:link-square-02" 
-              trailing size="sm" 
-              class="self-end" />
-          </div>
-          <div v-else class="grid items-center justify-center bg-gray-200 dark:bg-gray-800 rounded-md p-4">
-            <UButton
-              icon="hugeicons:plus-sign-circle"
-              label="Добавить комментарий" 
-              color="primary"
-              size="sm"
-              @click="openCommentPrompt" />
-          </div>
-        </template>
-      </UAccordion>
-    </div>
-
-    <CommentAddEntry
-      ref="commentPrompt"
-      :entry-id="item.documentId"
-      entry-name="customer"
-      @handle-comment="handleComment" />
-
-  </div>
-</template> -->
-
 <script setup>
 const route = useRoute()
 const props = defineProps({
@@ -164,20 +56,18 @@ const isEdit = computed(() => route.hash === '#edit')
 
       <div v-if="item.contact && item.contact.length" class="flex flex-col gap-2">
         <p class="text-sm opacity-70 px-1">Контакты</p>
-        <div 
-          v-for="(contactPerson, index) in item.contact" :key="index" 
-          class="grid gap-3 bg-gray-200 dark:bg-gray-900 rounded-lg p-3">
-          <div v-if="contactPerson.name || contactPerson.position" class="flex flex-col gap-1 text-gray-900 dark:text-white">
-            <span class="text-sm opacity-70">{{ contactPerson.position || 'Контактное лицо' }}</span>
-            <span class="font-bold">{{ contactPerson.name }}</span>
+        <div v-for="(person, i) in item.contact" :key="i" class="grid gap-3 bg-gray-200 dark:bg-gray-900 rounded-lg p-3">
+          <div v-if="person.name || person.position" class="flex flex-col gap-1 text-gray-900 dark:text-white">
+            <span class="text-sm opacity-70">{{ person.position || 'Контактное лицо' }}</span>
+            <span class="font-bold">{{ person.name }}</span>
           </div>
-          <div v-if="contactPerson.phone" class="flex flex-col gap-1 text-gray-900 dark:text-white">
+          <div v-if="person.phone" class="flex flex-col gap-1 text-gray-900 dark:text-white">
             <span class="text-sm opacity-70">Телефон</span>
-            <span class="font-bold">{{ contactPerson.phone }}</span>
+            <span class="font-bold">{{ person.phone }}</span>
           </div>
-          <div v-if="contactPerson.email" class="flex flex-col gap-1 text-gray-900 dark:text-white">
+          <div v-if="person.email" class="flex flex-col gap-1 text-gray-900 dark:text-white">
             <span class="text-sm opacity-70">Электронный адрес</span>
-            <span class="font-bold">{{ contactPerson.email }}</span>
+            <a :href="`mailto:${person.email}`" target="_blank" class="font-bold">{{ person.email }}</a>
           </div>
         </div>
       </div>
