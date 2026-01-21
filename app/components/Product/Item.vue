a28672fcde726f88eddd1db1d81f9cb1711d6aff
geniy9 <geniy9@gmail.com>
2026-01-21 19:18:54 +0500

    prod item truncate 28

--- app/components/Product/Item.vue
+++ app/components/Product/Item.vue
@@ -11,7 +11,7 @@ const props = defineProps({
   <NuxtLink :to="`/stock/${item?.documentId}`" 
     class="flex justify-between items-center cursor-pointer p-2 bg-gray-200 dark:bg-gray-900 rounded-lg">
     <h3 class="font-bold text-gray-900 dark:text-white">
-      {{ truncate(item.name, 30) }}
+      {{ truncate(item.name, 28) }}
     </h3>
     <UBadge v-if="item.expireDate" :color="expireDateStatus(item.expireDate)" size="sm">
       {{ humanDate(item.expireDate) }}
